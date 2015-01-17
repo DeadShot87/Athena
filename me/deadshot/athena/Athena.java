2429e510507b672c3bc2033e2fcb268b6c9cd11c
DeadShot87 <2414498479@qq.com>
2015-01-17 22:55:41 +0800

    Update Athena.java

--- me/deadshot/athena/Athena.java
+++ me/deadshot/athena/Athena.java
@@ -1,8 +1,27 @@
 package me.deadshot.athena;
 
-import org.bukkit.plugin.JavaPlugin;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public class Athena extends JavaPlugin
 {
-
+  public static Athena plugin;
+  
+  @Override
+  public void onLoad()
+  {
+    plugin=this;
+  }
+  
+  @Override
+  public void onEnable()
+  {
+    
+  }
+  
+  @Override
+  public void onDisable()
+  {
+    
+    
+  }
 }
